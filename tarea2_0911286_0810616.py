e6f064f2efd5d6e3a9520892993f1589e6652259
hazel0703 <apdr83@gmail.com>
2015-04-21 12:33:02 -0430

    Commit

--- tarea2_0911286_0810616.py
+++ tarea2_0911286_0810616.py
@@ -1,7 +1,48 @@
 '''
-Created on 16/4/2015
+Created on Apr 20, 2015
 
-@author: Adriana Devera 09-11286
+@author: Ricardo
 '''
+import unittest
+from calcularPrecio import *
+from datetime import datetime
+from test.test_deque import fail
 
-print("Hola Mundo")
+class calcularPrecioTest(unittest.TestCase):
+
+
+    def testTarifaNegativa(self):
+        Tar = Tarifa(-100,-200)
+        tiempo = [datetime.strptime("15-04-10/15:30","%y-%m-%d/%H:%M"), datetime.strptime("15-04-12/16:38","%y-%m-%d/%H:%M")]
+        try:
+            calcularPrecio(Tar, tiempo)
+            fail("El metodo debio generar error!")
+        except Exception:
+            pass
+        
+    def testMaximoReserva(self):
+        Tar = Tarifa(100,200)
+        tiempo = [datetime.strptime("15-04-10/15:30","%y-%m-%d/%H:%M"), datetime.strptime("15-04-25/16:38","%y-%m-%d/%H:%M")]
+        try:
+            calcularPrecio(Tar, tiempo)
+            fail("El metodo debio generar error!")
+        except Exception:
+            pass
+        
+         
+    def testMinimoReserva(self):
+        Tar = Tarifa(100,200)
+        tiempo = [datetime.strptime("15-04-10/15:30","%y-%m-%d/%H:%M"), datetime.strptime("15-04-10/15:40","%y-%m-%d/%H:%M")]
+        try:
+            calcularPrecio(Tar, tiempo)
+            fail("El metodo debio generar error!")
+        except Exception:
+            pass
+        
+   
+   
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
